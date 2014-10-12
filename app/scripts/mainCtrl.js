2e200b65b0c74beea55e9a3f693e486f0868fd6e
Kim Pettersen <kim@brightbytes.net>
2014-10-12 13:51:47 -0700

    started styling

--- app/scripts/mainCtrl.js
+++ app/scripts/mainCtrl.js
@@ -2,6 +2,31 @@
 
 angular.module('controller.main', [])
   .controller('MainCtrl', function($scope, RestService) {
-    $scope.name = 'kim'
+    $scope.showErrorMessage = false;
+    $scope.errorMessage = '';
+    var s = skrollr.init({
+        render: function(data) {
+            console.log(data.curTop);
+        }
+    });
+    $scope.populate = function() {
+      RestService.experience
+        .query()
+        .$promise
+        .then(function(experience) {
+
+           $scope.experience = experience;
+        }).catch($scope.errorHandler);
+
+      console.log($scope.experience);
+    };
+
+    $scope.errorHandler = function(err) {
+      $scope.showErrorMessage = true;
+      $scope.errorMessage = err;
+    };
+
+    $scope.populate();
+
   });
 
